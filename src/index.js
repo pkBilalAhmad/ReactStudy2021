6b4bfafd9e921c6c6277cd9a35709e83d7b901e7
Bilal Ahmed <pk92love@gmail.com>
2021-11-24 12:11:31 +0500

    React Clock added syncronous level

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-render() {
-  return (
-    <div>
-      
-    </div>
-  )
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: new Date() }
+  }
+  componentDidMount() {
+    this.tim = setInterval(() => this.tick(), 1000)
+  }
+  tick() {
+    this.setState({
+      time: new Date()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>This is time in syncronous point</h1>
+        <h2>Now the time is </h2>
+        <h3>{this.state.time.toLocaleTimeString()}</h3>
+      </div>
+    );
+  }
 }
 
-this.setState();
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+)
