79cd1f0507f50881bf756c603fd15632e4f4f8a0
Bilal Ahmed <pk92love@gmail.com>
2021-11-24 17:36:10 +0500

    TextCounter repaired and learned the 3 method of binding class methods

--- src/countIncrement.js
+++ src/countIncrement.js
@@ -5,18 +5,26 @@ class TextCounter extends React.Component {
     constructor(props) {
         super(props);
         this.state = { text: "Text here to show your interst" }
+        // this.inc = this.inc.bind(this)
+        /*
+            3 ways to bind class method 1 bind this in constructor
+            2 assign that function ta variable
+            and call this without () with this object 
+            3 add calling in event after errow funtion with () but when the function is simple and not incloded inner funtion
+        */
     }
-    inc(e) {
-        this.setState({
+    inc = (e) => {
+        this.setState(() => ({
             text: e.target.value
-        });
+        }));
     }
     render() {
         return (
             <div>
                 <h1>Hello there is Bilal Ahmed</h1>,
                 <h2>Texting</h2>,
-                <textarea defaultValue={this.state.text} onChange={this.inc} ></textarea>
+                <textarea value={this.state.text} onChange={this.inc} />
+                {/* <textarea value={this.state.text} onChange={this.inc} /> Hello*/}
                 <h2>{this.state.text}</h2>
             </div>
         );
