99d331a00e5aa7888897c47bf9d34727ff763b37
Bilal Ahmed <pk92love@gmail.com>
2021-12-01 22:19:46 +0500

    todo app fairst stage with problme

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -3,19 +3,45 @@ import React from 'react';
 class TodoApp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = { todoList: [], todoText: '' }
+    }
+    changeValue = (e) => {
+        this.setState({
+            todoText: this.state.todoText + e.target.value
+        });
+    }
+    _submit = (event) => {
+        event.preventDefault()
+        this.setState({
+            todoList: this.state.todoText
+        });
     }
     render() {
-        return ( 
+        return (
             <div>
+                <form onSubmit={this._submit}>
+                    {/* <input value="" /> */}
+                    <button type="submit">Submit Item</button>
+                    <input value='' onChange={this.changeValue} />
+                </form>
+
                 <ul>
                     <li>First Name </li>
                     <li>Last Name </li>
-                    <li>Name Bilal </li>
-                    <li>Bilal Ahmed</li>
+                    {this.state.todoList.map((item) => (
+                        <li>{item}</li>
+                    ))}
                 </ul>
+                {/* <ol>
+                    {this.state.todoList.map((item) => (
+                        <li>{item}</li>
+                    ))}
+                </ol> */}
+                <h2>
+                    {this.state.todoText}
+                </h2>
             </div>
-         );
+        );
     }
 }
 
