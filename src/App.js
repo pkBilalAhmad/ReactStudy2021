b703d64da70afaead070ed7554e022a852ff4c8f
Bilal Ahmed <pk92love@gmail.com>
2021-12-01 13:50:48 +0500

    fancyTable componenet code added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,31 @@ class Table extends React.Component {
         this.state = { data: this.props.data }
         this._sort = this._sort.bind(this)
     }
+    content (){
+        var headers = [
+            "Book", "Author", "Language", "Published", "Sales"
+        ];
+        var data = [
+            ["The Lord of the Rings", "J. R. R. Tolkien",
+                "English", "1954–1955", "150 million"],
+            ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupéry",
+                "French", "1943", "140 million"],
+            ["Harry Potter and the Philosopher's Stone", "J. K. Rowling",
+                "English", "1997", "107 million"],
+            ["And Then There Were None", "Agatha Christie",
+                "English", "1939", "100 million"],
+            ["Dream of the Red Chamber", "Cao Xueqin",
+                "Chinese", "1754–1791", "100 million"],
+            ["The Hobbit", "J. R. R. Tolkien",
+                "English", "1937", "100 million"],
+            ["She: A History of Adventure", "H. Rider Haggard",
+                "English", "1887", "100 million"]
+        ];
+        return {
+            headers,
+            data
+        }
+    }
     _sort(e) {
         let column = e.target.cellIndex;
         let data = this.state.data.slice();
@@ -24,7 +49,7 @@ class Table extends React.Component {
                 <table>
                     <thead onClick={this._sort}>
                         <tr>
-                            {this.props.headers.map((item) => (
+                            {this.content.bind(this).map((item) => (
                                 <th key={item} style={{ border: '2px solid red' }} >{item}</th>
                             ))}
                         </tr>
@@ -41,30 +66,30 @@ class Table extends React.Component {
         );
     }
 }
-function DataList() {
-    var headers = [
-        "Book", "Author", "Language", "Published", "Sales"
-    ];
-    var data = [
-        ["The Lord of the Rings", "J. R. R. Tolkien",
-            "English", "1954–1955", "150 million"],
-        ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupéry",
-            "French", "1943", "140 million"],
-        ["Harry Potter and the Philosopher's Stone", "J. K. Rowling",
-            "English", "1997", "107 million"],
-        ["And Then There Were None", "Agatha Christie",
-            "English", "1939", "100 million"],
-        ["Dream of the Red Chamber", "Cao Xueqin",
-            "Chinese", "1754–1791", "100 million"],
-        ["The Hobbit", "J. R. R. Tolkien",
-            "English", "1937", "100 million"],
-        ["She: A History of Adventure", "H. Rider Haggard",
-            "English", "1887", "100 million"]
-    ];
-    return (
-        <div> <Table headers={headers} data={data} />
-        </div>
-    )
-}
+// function DataList() {
+//     var headers = [
+//         "Book", "Author", "Language", "Published", "Sales"
+//     ];
+//     var data = [
+//         ["The Lord of the Rings", "J. R. R. Tolkien",
+//             "English", "1954–1955", "150 million"],
+//         ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupéry",
+//             "French", "1943", "140 million"],
+//         ["Harry Potter and the Philosopher's Stone", "J. K. Rowling",
+//             "English", "1997", "107 million"],
+//         ["And Then There Were None", "Agatha Christie",
+//             "English", "1939", "100 million"],
+//         ["Dream of the Red Chamber", "Cao Xueqin",
+//             "Chinese", "1754–1791", "100 million"],
+//         ["The Hobbit", "J. R. R. Tolkien",
+//             "English", "1937", "100 million"],
+//         ["She: A History of Adventure", "H. Rider Haggard",
+//             "English", "1887", "100 million"]
+//     ];
+//     return (
+//         <div> <Table headers={headers} data={data} />
+//         </div>
+//     )
+// }
 
-export default DataList;
+export default Table;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import Table from './App'
 
 
 
 ReactDOM.render(
-  <App />,
+  <Table />,
   document.getElementById('root')
 )
