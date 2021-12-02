4eb634629f7f14971fce5c7d47f193c0b0d49f8e
Bilal Ahmed <pk92love@gmail.com>
2021-12-02 13:39:12 +0500

    todo array added in state n so on abject in array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoApp from './TodoApp'
+// import TodoApp from './TodoApp'
 import TextCounter from './textCounter'
 
 
--- src/textCounter.js
+++ src/textCounter.js
@@ -3,29 +3,35 @@ import React from 'react';
 class TextCounter extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { text: '', todo: ["Bilal",'Ahmed'] }
+        this.state = {
+            text: '', todo: [
+                { name: 'Bilal', city: 'Karachi', compoleted: false },
+                { name: 'Fazal', city: 'Lahor' }
+            ]
+        }
     }
     changValue = (e) => {
         this.setState({
             text: e.target.value
         });
     }
-    addTodo = ()=>{
+    addTodo = () => {
         this.setState({
-            todo : this.state.todo.concat(this.state.text),
-            text : ''
+            todo: this.state.todo.concat({ name: this.state.text, city: this.state.text }),
+            text: ''
         });
     }
     render() {
         return (<div>
             <h1>{this.state.todo.length}</h1>
-            <button onClick={this.addTodo}>Add Todo</button><br/>
+            <button onClick={this.addTodo}>Add Todo</button><br />
             <textarea value={this.state.text} onChange={this.changValue} />
             {/* <h1>{this.state.text}</h1> */}
             <h3>{this.state.text.length}</h3>
             <ul>
-                {this.state.todo.map((item , idx)=>(
-                    <li key={idx}>{item}</li>
+                {this.state.todo.map((item, idx) => (
+                    <li key={idx}>Name = : {item.name} <br /> City =: {item.city} </li>
+
                 ))}
             </ul>
         </div>);
